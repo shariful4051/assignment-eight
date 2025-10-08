77ffa35845fadacc66230c92ea3b96ef59be450a
shariful4051 <sorif19219004051@gmail.com>
2025-10-08 11:45:25 +0100

    Details part done with rechart

--- src/assets/component/Pages/CardDetails.jsx
+++ src/assets/component/Pages/CardDetails.jsx
@@ -1,18 +1,87 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
+import Download from '../../../assets/icon-downloads.png'
+import Review from '../../../assets/icon-review.png'
+import Rating from '../../../assets/icon-ratings.png'
+import { Bar, BarChart, XAxis, YAxis } from 'recharts';
+
 
 const CardDetails = () => {
     const{id} = useParams()
     const appId =parseInt(id)
-    console.log(id);
+   // console.log(id);
 
     const apps =useLoaderData()
     const singleApp = apps.find(app=>app.id ==appId)
-    
+    const rating = singleApp.ratings;
+    console.log(rating);
     return (
         <div>
-            <img src={singleApp.image} alt="" />
-            <h1>{singleApp.title}</h1>
+
+
+        <div className='flex gap-10 md:flex-row flex-col'>
+            <div>
+                <img className='h-[350px] w-[350px]' src={singleApp.image} alt="" />
+            </div>
+
+           <div className='flex flex-col  justify-between'>
+
+             <div>
+                <h1 className='font-bold text-3xl text-black'>{singleApp.title}</h1>
+                <p><span className='text-[#627382]'>Developed By </span> <span className='text-[#632EE3]'>{singleApp.companyName}</span></p>
+            </div>
+
+
+            <div className='flex  gap-6'>
+                <div className='flex flex-col'>
+                    <span><img src={Download} alt="" /></span>
+                    <span>Downloads</span>
+                    <span className='font-extrabold text-4xl'>8M</span>
+                </div>
+                <div className='flex flex-col'>
+                    <span><img src={Rating} alt="" /></span>
+                    <span>Average Ratings</span>
+                    <span className='font-extrabold text-4xl'>4.9</span>
+                </div>
+                <div className='flex flex-col'>
+                    <span><img src={Review} alt="" /></span>
+                    <span>Total Reviews</span>
+                    <span className='font-extrabold text-4xl'>54K</span>
+                </div>
+                
+            </div>
+
+                  <div>
+                    <button className='bg-[#00D390] text-white font-semibold px-5 py-3.5 rounded-md'>Install Now({singleApp.size}MB)</button>
+                </div>
+
+           </div>
+             
+            
+        </div>
+
+        {/* rechart */}
+        <div className='my-10'>
+            <h1 className='font-semibold text-2xl my-3'>Ratings</h1>
+            <div>
+             <BarChart width={400} height={400} data={rating}>
+                <XAxis dataKey={'name'}></XAxis>
+                <YAxis ></YAxis>
+                <Bar dataKey={'count'} fill='green'></Bar>
+                
+
+             </BarChart>
+  
+            </div>
+            
+        </div>
+
+        <div>
+            <h1 className='font-semibold text-2xl text-black'>Description</h1>
+            <p>{singleApp.description}</p>
+        </div>
+
+
         </div>
     );
 };
@@ -21,26 +90,3 @@ export default CardDetails;
 
 
 
-
-
-// const BookDetails = () => {
-//     const {id} = useParams()
-//     const bookId = parseInt(id)
-//     const data = useLoaderData();
-//     const singleBook = data.find(book=>book.bookId ===bookId)
-//     console.log(singleBook)
-//    // console.log(data);
-//     //console.log(id,bookId);
-//    const {bookName,image} = singleBook;
-
-//    const handle =(id)=>{
-//     addToLs(id)
-//    }
-//     return (
-//         <div>
-//             <img className='h-[100px]' src={image} alt="" />
-//             <p>{bookName}</p>
-//             <button onClick={()=>handle(id)} className='cursor-pointer bg-amber-200 rounded-lg px-3 py-2 text-green-800 font-bold '>Read</button>
-//             <button className='ml-2 cursor-pointer bg-amber-200 rounded-lg px-3 py-2 text-green-800 font-bold '>Wish list</button>
-//         </div>
-//     );
